21022842e038850870171865677473baa7093fff
Ryan <ryanmfloersch@gmail.com>
2023-03-16 16:42:51 -0400

    Restructure the html, redid styles in css, and restarting the js

---
+++ assets/js/questions.js
@@ -0,0 +1,25 @@
+// An array of objects containing the questions
+var questions = [
+    {
+        question: "What data type only has the values true and false?",
+        // questionNumber: 1,
+        ans1: "1. boolean",
+        ans2: "2. string",
+        ans3: "3. number",
+        ans4: "4. undefined",
+        correctAnswer: "1",
+
+    },
+    {
+        question: "What does the DOM stand for?",
+        // questionNumber: 2,
+        ans1: "1. Dominoe On Marketplace",
+        ans2: "2. Document Object Model",
+        ans3: "3. Dodging Oncoming Motorcyles",
+        ans4: "4. Deep Ocean Maintainance",
+        correctAnswer: "2",
+
+        
+    }
+];
+
